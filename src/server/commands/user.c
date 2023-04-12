7bef7297fc5503bbcc4353b26fe2173f6094b7ee
Simon <simon.riembault@gmail.com>
2023-04-12 14:00:36 +0200

    [~] Moving command func in different files

--- src/server/commands/user.c
+++ src/server/commands/user.c
@@ -58,34 +58,3 @@ void give_users(server_t *server, client_t *client, string data)
     packet_t *packet = create_packet(LOGIN_SUCCESS, "test");
     send_packet(client->socket_fd, packet);
 }
-
-bool correct_uuid(string uuid, server_t *server)
-{
-    if (strlen(uuid) != 36)
-        return false;
-    for (node *users = server->users; users != NULL; users = users->next) {
-        if (((client_t *)users->data)->user->uuid != NULL && strcmp(
-            ((client_t *)users->data)->user->uuid, uuid) == 0)
-            return true;
-    }
-    return false;
-}
-
-void give_user_info(server_t *server, client_t *client, string data)
-{
-    int i = 0;
-    string msg_cli = NULL;
-    packet_t *packet;
-    char **command = str_to_word_array(data, " \t");
-    for (i; command[i] != NULL; i++);
-    if (i != 2) {
-        packet = create_packet(ERROR, "invalid number of arguments");
-    } else if (correct_uuid(command[1], server)) {
-        msg_cli = MALLOC(sizeof(char) * strlen(command[1]) + 1);
-        sprintf(msg_cli, "test %s", command[1]);
-        packet = create_packet(LOGIN_SUCCESS, msg_cli);
-    } else {
-        packet = create_packet(LOGIN_SUCCESS, "wrong uuid");
-        send_packet(client->socket_fd, packet);
-    }
-}
