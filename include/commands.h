9e67f12a6f1632f0e42271d1ed57d1dcb4198baf
Simon <simon.riembault@gmail.com>
2023-04-12 11:44:55 +0200

    [-] Adding basic /user command but cannot test because don't have the correct version

--- include/commands.h
+++ include/commands.h
@@ -31,8 +31,10 @@ void use(server_t *server, client_t *client, string data);
 void create(server_t *server, client_t *client, string data);
 void list_team(server_t *server, client_t *client, string data);
 void give_info(server_t *server, client_t *client, string data);
+void display_help(server_t *server, client_t *client, string data);
 
 static const command commands[] = {
+    {display_help, "/help", 0, "show help", NONE},
     {login_user, "/login", 0, "set the user_name used by client", NONE},
     {logout_user, "/logout", 0, "disconnect the client from the server", NONE},
     {give_users, "/users", 0, "get the list of all users that exist on the\
--- src/client/listeners/sucess.c
+++ src/client/listeners/sucess.c
@@ -6,6 +6,7 @@
 */
 
 #include "client.h"
+#include "logging_client.h"
 
 /**
  * Success listener.
@@ -13,7 +14,9 @@
  */
 void success(packet_t *packet)
 {
+    //sscanf(packet->data, "%s|%s|%s", client_uuid, client_name, message);
     printf("Info: %s\n", packet->data);
+    //client_event_logged_in()
 }
 
 /**
